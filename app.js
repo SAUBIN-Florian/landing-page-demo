611b9e332f8fdd9e19211ff5353069613ead6b4d
floriansaubin <saubinflorian@gmail.com>
2021-10-08 11:30:06 +0200

    opacity animation JS added

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+// OPACITY ANIMATION
+const ratio = 0.1;
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.2
+};
 
+const handleIntersect = (entries, observer) => {
+  entries.forEach((entry)=>{
+    if(entry.intersectionRatio > ratio){
+      entry.target.classList.add("reveal-visible")
+      observer.unobserve(entry.target);
+    }
+  })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll(".reveal").forEach((r)=>{
+  observer.observe(r);
+})
 
 // MAPBOX API
 
