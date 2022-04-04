0a01936e6b8bbc21b76bd54bcf2210c8f01f0571
floriansaubin <saubinflorian@gmail.com>
2022-04-04 11:11:48 +0200

    start improve perfs issues

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ document.querySelectorAll('[class*="reveal-"]').forEach((r)=>{
 // MAPBOX API
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicy1mbG9yaWFuIiwiYSI6ImNrbGt3cjYxazBycG8ycm9iOWgzbXEyOGIifQ.XvkhPvp5NJCloXP3atH25w';
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/outdoors-v11',
   center: [4.833350965586533, 45.76423780141141],
